ae7b056d8fb723e5161a8ead7e32c4a3771b49cd
Johan Hellgren <honajnahoj@gmail.com>
2024-08-25 20:00:45 +0200

    init

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+return { 
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
+    default_integrations = true
+    end
+  }
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,17 @@
+return {
+  'rcarriga/nvim-notify',
+  config = function()
+    vim.notify = require('notify')
+    vim.notify.setup({
+      stages = "fade_in_slide_out",
+      timeout = 5000,
+      icons = {
+        ERROR = "",
+        WARN = "",
+        INFO = "",
+        DEBUG = "",
+        TRACE = "✎",
+      },
+    })
+  end,
+}
