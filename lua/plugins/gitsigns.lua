fa5c97451cdf2b518d5e5e719ca660ecf8ba0009
Johan Hellgren <honajnahoj@gmail.com>
2024-08-27 19:28:00 +0200

    config gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,15 +5,63 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
-      local gs = require("gitsigns")
-      local wk = require("which-key")
-      wk.add({
-        {"<leader>gb", gs.toggle_current_line_blame, desc = "Toggle line blame"},
-        {"<leader>gh", gs.preview_hunk, desc = "Preview hunk"},
-        {"<leader>gd", gs.diffthis, desc = "Diff this"},
-        {"<leader>gD", gs.toggle_deleted, "Show deleted"},
-      })
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged_enable = true,
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					follow_files = true,
+				},
+				auto_attach = true,
+				attach_to_untracked = false,
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 0,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+				},
+				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+				preview_config = {
+					-- Options passed to nvim_open_win
+					border = "single",
+					style = "minimal",
+					relative = "cursor",
+					row = 0,
+					col = 1,
+				},
+			})
+			local gs = require("gitsigns")
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>gb", gs.toggle_current_line_blame, desc = "Toggle line blame" },
+				{ "<leader>gh", gs.preview_hunk, desc = "Preview hunk" },
+				{ "<leader>gd", gs.diffthis, desc = "Diff this" },
+				{ "<leader>gD", gs.toggle_deleted, desc = "Show deleted" },
+			})
 		end,
 	},
 }
