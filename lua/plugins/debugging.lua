cbc360bdc35d0b7ef8f65193733a6ad224fe4c64
JohanHellgren <honajnahoj@gmail.com>
2024-09-02 14:10:58 +0200

    Update debugging keys

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -20,16 +20,16 @@ return {
     end
     local wk = require("which-key")
     wk.add({
-      { "<leader>x",  group = "Debugging" },
-      { "<leader>xb", dap.toggle_breakpoint, desc = "Toggle breakpoint" },
-      { "<leader>xd", dap.step_over,         desc = "Step over" },
-      { "<leader>xi", dap.step_into,         desc = "Step into" },
-      { "<leader>xo", dap.step_out,          desc = "Step out" },
-      { "<leader>xr", dap.repl.open,         desc = "Open REPL" },
-      { "<leader>xs", dap.start,             desc = "Start debugging" },
-      { "<leader>xS", dap.stop,              desc = "Stop debugging" },
-      { "<leader>xc", dap.continue,          desc = "Continue" },
-      { "<leader>xz", dap.run_last,          desc = "Run last" },
+      { "<leader>d",  group = "Debugging" },
+      { "<leader>db", dap.toggle_breakpoint, desc = "Toggle breakpoint" },
+      { "<leader>dd", dap.step_over,         desc = "Step over" },
+      { "<leader>di", dap.step_into,         desc = "Step into" },
+      { "<leader>do", dap.step_out,          desc = "Step out" },
+      { "<leader>dr", dap.repl.open,         desc = "Open REPL" },
+      { "<leader>ds", dap.start,             desc = "Start debugging" },
+      { "<leader>dS", dap.stop,              desc = "Stop debugging" },
+      { "<leader>dc", dap.continue,          desc = "Continue" },
+      { "<leader>dz", dap.run_last,          desc = "Run last" },
     })
   end,
 }
